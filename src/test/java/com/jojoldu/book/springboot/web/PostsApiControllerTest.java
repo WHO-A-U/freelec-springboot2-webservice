68a663a0f52c57ca6ae6c4c31a8211d320da09cb
WHO-A-U <59171080+WHO-A-U@users.noreply.github.com>
2021-04-08 03:17:40 +0900

    posts 등록에 대한 test 코드작성

---
+++ src/test/java/com/jojoldu/book/springboot/web/PostsApiControllerTest.java
@@ -0,0 +1,77 @@
+package com.jojoldu.book.springboot.web;
+
+import com.jojoldu.book.springboot.domain.posts.Posts;
+import com.jojoldu.book.springboot.domain.posts.PostsRepository;
+import com.jojoldu.book.springboot.web.dto.PostsSaveRequestDto;
+import org.junit.After;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class PostsApiControllerTest {
+
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+    //JPA의 기능까지 한번에 테스트 하기 떄문에 WebMvcTest 대신
+    //SpringBootTest 와 TestRestTemplate을 함께 사용해주면 된다.
+
+    @Autowired
+    private PostsRepository postsRepository;
+
+    @After
+    public void tearDown() throws Exception{
+        postsRepository.deleteAll();
+    }
+
+    @Test
+    public void posts_등록된다() throws Exception{
+
+        String title = "title";
+        String content = "content";
+
+        PostsSaveRequestDto requestDto = PostsSaveRequestDto.builder()
+                .title(title)
+                .content(content)
+                .author("me")
+                .build();
+
+        //RequestBody어노테이션으로 요청받을때 DTO가 생성되는 부분을
+        //테스트 니까 builder를 이용하여 만들어준다
+
+        String url = "http://localhost:"+port+"/api/v1/posts";
+
+        //언제
+        //ResponseEntity 란 스프링부트에서 제공하는 http response 객체
+        //이 안에는 header , body , status 등등 다른 객체들이 들어있음
+        ResponseEntity<Long> responseEntity = restTemplate.postForEntity(url,requestDto,Long.class);
+
+        //받으면 비교해보기
+
+        assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
+
+        assertThat(responseEntity.getBody()).isGreaterThan(0L);
+
+        List<Posts> all = postsRepository.findAll();
+        assertThat(all.get(0).getTitle()).isEqualTo(title);
+
+        assertThat(all.get(0).getContent()).isEqualTo(content);
+
+
+    }
+
+}
+
