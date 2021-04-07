6d07164673ed72b4d7ac987b3d23fdef0c693659
WHO-A-U <59171080+WHO-A-U@users.noreply.github.com>
2021-04-08 03:10:47 +0900

    define DAO lombok 이용한 정의

---
+++ src/main/java/com/jojoldu/book/springboot/web/dto/PostsSaveRequestDto.java
@@ -0,0 +1,30 @@
+package com.jojoldu.book.springboot.web.dto;
+
+import com.jojoldu.book.springboot.domain.posts.Posts;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class PostsSaveRequestDto {
+    private String title;
+    private String content;
+    private String author;
+
+    @Builder //build 패턴은 setter 를 없는 객체를 생성하여 변경불가능하는데에 의미를 가진다.
+    public PostsSaveRequestDto(String title, String content, String author) {
+        this.title = title;
+        this.content = content;
+        this.author = author;
+    }
+
+    public Posts toEntity() {
+        return Posts.builder()
+                .title(title)
+                .content(content)
+                .author(author)
+                .build();
+    }
+
+}
