654c597c445f3d47c06fccf1a181b4dbfb162670
WHO-A-U <59171080+WHO-A-U@users.noreply.github.com>
2021-04-07 17:22:55 +0900

    application 생성

---
+++ src/main/java/com/jojoldu/book/springboot/Application.java
@@ -0,0 +1,12 @@
+package com.jojoldu.book.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args){
+        SpringApplication.run(Application.class,args);
+        //SpringApplication을 통해 외부 톰캣(WAS)를 사용하지 않고 내부 WAS를 실행
+        //gradle에 잘보면 Dependencies 에 tomcat이 존재함
+    }
+}
