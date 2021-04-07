56fee1dcbc199a756797b34e5c5b9efd4580f9ac
WHO-A-U <59171080+WHO-A-U@users.noreply.github.com>
2021-04-07 19:26:51 +0900

    DTO + controller

---
+++ src/main/java/com/jojoldu/book/springboot/web/dto/HelloResponseDto.java
@@ -0,0 +1,15 @@
+package com.jojoldu.book.springboot.web.dto;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+//선언된 모든 final 필드나 @NonNull 이포함된 필드에 대해서서생성자를 생성해 줍니다.
+//
+@Getter
+@RequiredArgsConstructor
+public class HelloResponseDto {
+
+    private final String name;
+    private final int amount;
+
+}
+
