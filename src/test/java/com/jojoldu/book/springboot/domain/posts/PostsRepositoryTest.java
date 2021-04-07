7ea69a982a2c65a219cdf3b1479cefd297b642f6
WHO-A-U <59171080+WHO-A-U@users.noreply.github.com>
2021-04-07 21:15:29 +0900

    테스트 코드 + sql log 확인하기 위한 properties 설정

---
+++ src/test/java/com/jojoldu/book/springboot/domain/posts/PostsRepositoryTest.java
@@ -0,0 +1,46 @@
+package com.jojoldu.book.springboot.domain.posts;
+
+import org.junit.After;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PostsRepositoryTest {
+
+    @Autowired
+    PostsRepository postsRepository;
+
+    @After
+    public void cleanup(){ //초기화를 통해 매번 동일한 조건의 테스트를 가능하게 함
+        postsRepository.deleteAll();
+    }
+
+    @Test
+    public void 게시글저장_불러오기(){
+
+        String title = "테스트 게시글";
+        String content = "테스트 본문";
+
+        postsRepository.save(Posts.builder()  //id값 유무에 따라 update or insert 가 실행됨
+                .title(title)
+                .content(content)
+                .author("my@email.com")
+                .build());
+
+        List<Posts> postsList = postsRepository.findAll(); //모든 데이터를 조회
+
+        Posts posts = postsList.get(0);
+        assertThat(posts.getTitle()).isEqualTo(title);
+        assertThat(posts.getContent()).isEqualTo(content);
+    }
+
+
+}
