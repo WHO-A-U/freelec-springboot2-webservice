a19503785e4e336247dd4d7696bb92ef450d0444
WHO-A-U <59171080+WHO-A-U@users.noreply.github.com>
2021-04-08 17:35:50 +0900

    update , get  구현

--- src/main/java/com/jojoldu/book/springboot/service/PostsService.java
+++ src/main/java/com/jojoldu/book/springboot/service/PostsService.java
@@ -1,6 +1,8 @@
 package com.jojoldu.book.springboot.service;
 
+import com.jojoldu.book.springboot.domain.posts.Posts;
 import com.jojoldu.book.springboot.domain.posts.PostsRepository;
+import com.jojoldu.book.springboot.web.dto.PostsResponseDto;
 import com.jojoldu.book.springboot.web.dto.PostsSaveRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -14,5 +16,20 @@ public class PostsService {
     @Transactional //service에서 트랜잭션 처리를 해주어야 된다. ACID 원칙.
     public Long save(PostsSaveRequestDto requestDto){
         return postsRepository.save(requestDto.toEntity()).getId();
+    }
+
+    @Transactional
+    public Long update(Long id , PostsSaveRequestDto requestDto){
+        Posts posts = postsRepository.findById(id).orElseThrow(()->new IllegalArgumentException("해당 사용자가 없습니다. id="+id));
+        posts.update(requestDto.getTitle(),requestDto.getContent());
+
+        return id;
+    }
+
+    @Transactional
+    public PostsResponseDto findById(Long id){
+        Posts entity = postsRepository.findById(id).orElseThrow(()->new IllegalArgumentException("해당 사용자가 없습니다. id="+id));
+
+        return new PostsResponseDto(entity);
     }
 }
