656ecf00d52e50c2d8d6da13d3bad8bd7f02e16e
WHO-A-U <59171080+WHO-A-U@users.noreply.github.com>
2021-04-08 21:30:28 +0900

    모든 목록 조회 구현

--- src/main/java/com/jojoldu/book/springboot/domain/posts/PostsRepository.java
+++ src/main/java/com/jojoldu/book/springboot/domain/posts/PostsRepository.java
@@ -1,7 +1,14 @@
 package com.jojoldu.book.springboot.domain.posts;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+
 
 public interface PostsRepository extends JpaRepository<Posts,Long>{
     //extends < [Entity class type] , [PK type] > 을 지정해주면 된다.
+    @Query("SELECT p FROM Posts p ORDER BY p.id DESC")
+    //SPringDataJpa 에서 제공되는 메서드가 아닐경우 @Query 를 이용하여 작성함
+    List<Posts> findAllDesc();
 }
--- src/main/java/com/jojoldu/book/springboot/service/PostsService.java
+++ src/main/java/com/jojoldu/book/springboot/service/PostsService.java
@@ -2,11 +2,16 @@ package com.jojoldu.book.springboot.service;
 
 import com.jojoldu.book.springboot.domain.posts.Posts;
 import com.jojoldu.book.springboot.domain.posts.PostsRepository;
+import com.jojoldu.book.springboot.web.dto.PostsListResponseDto;
 import com.jojoldu.book.springboot.web.dto.PostsResponseDto;
 import com.jojoldu.book.springboot.web.dto.PostsSaveRequestDto;
+import com.jojoldu.book.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -19,7 +24,7 @@ public class PostsService {
     }
 
     @Transactional
-    public Long update(Long id , PostsSaveRequestDto requestDto){
+    public Long update(Long id , PostsUpdateRequestDto requestDto){
         Posts posts = postsRepository.findById(id).orElseThrow(()->new IllegalArgumentException("해당 사용자가 없습니다. id="+id));
         posts.update(requestDto.getTitle(),requestDto.getContent());
 
@@ -31,5 +36,12 @@ public class PostsService {
         Posts entity = postsRepository.findById(id).orElseThrow(()->new IllegalArgumentException("해당 사용자가 없습니다. id="+id));
 
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc(){
+        return postsRepository.findAllDesc().stream()
+                .map(posts->new PostsListResponseDto(posts))
+                .collect(Collectors.toList());
     }
 }
